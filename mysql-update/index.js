83cad31bcda81ba6468fb2f53549179e6480dc31
Eduardo Landa <eduardo.landa@outlook.com>
2019-02-09 13:31:21 -0800

    error update

--- mysql-update/index.js
+++ mysql-update/index.js
@@ -8,14 +8,14 @@ app.use(bodyParser())
 app.use(cors())
 
 app.use(async ctx => {
-  // const data = await ctx.request.body.title
-  const item = await getName(watch)
+  const data = await ctx.request.body.title
+  const item = await getName(data)
   ctx.body = item;
 })
 
 async function getName(watch) {
   try {
-    const itemData = await pool.query(`UPDATE movieList SET movieWatched = ${watch} WHERE movieID = 2   `)
+    const itemData = await pool.query(`UPDATE movieList SET movieWatched = "${watch}" WHERE movieID = 2   `)
     return itemData
   } catch (error) {
     console.log(error)
